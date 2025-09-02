3dabdf0daa1997c575da5d9de46d4d4067891be5
choii4924 <choii4924@naver.com>
2025-09-02 15:10:42 +0900

    Add files via upload

---
+++ 03_Condition/src/com/kh/condition/Practice1.java
@@ -0,0 +1,19 @@
+package com.kh.condition;
+
+import java.util.Scanner;
+
+public class Practice1 {
+
+	public static void main(String[] args) {
+		method1();
+	}
+		// TODO Auto-generated method stub
+		public static void method1() {
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.print("메뉴 번호를 누르면 oo 메뉴입니다. : ");
+			int n = sc.nextInt();
+
+}
+
+}
