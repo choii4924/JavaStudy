30d66b79d72d3ce0389ff030975b7632e0fca4be
choii4924 <choii4924@naver.com>
2025-09-02 15:13:54 +0900

    Add files via upload

---
+++ 12_Collection/src/com/kh/collection/map/model/vo/Snack.java
@@ -0,0 +1,42 @@
+package com.kh.collection.map.model.vo;
+
+public class Snack {
+
+	private String flavor;   // 맛
+	private int calory;      // 칼로리
+
+	// 기본생성자
+	public Snack() {
+		super();
+	}
+
+	// 모든 필드를 매개변수로 가지는 생성자
+	public Snack(String flavor, int calory) {
+		super();
+		this.flavor = flavor;
+		this.calory = calory;
+	}
+
+	// getter / setter
+	public String getFlavor() {
+		return flavor;
+	}
+
+	public void setFlavor(String flavor) {
+		this.flavor = flavor;
+	}
+
+	public int getCalory() {
+		return calory;
+	}
+
+	public void setCalory(int calory) {
+		this.calory = calory;
+	}
+
+	// toString 오버라이딩
+	@Override
+	public String toString() {
+		return "Snack [flavor=" + flavor + ", calory=" + calory + "]";
+	}
+}
