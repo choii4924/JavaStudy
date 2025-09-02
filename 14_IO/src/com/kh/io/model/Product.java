9de090697f6532985daf2addc6bd72b4c288364f
choii4924 <choii4924@naver.com>
2025-09-02 15:14:28 +0900

    Add files via upload

---
+++ 14_IO/src/com/kh/io/model/Product.java
@@ -0,0 +1,44 @@
+package com.kh.io.model;
+
+import java.util.Objects;
+
+public class Product {
+	private String name;
+	private int price;
+	
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, price);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(name, other.name) && price == other.price;
+	}
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + "]";
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+}
