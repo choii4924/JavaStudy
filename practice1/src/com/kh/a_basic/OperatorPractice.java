a711bd5d9076ec936f7d6e9dc41cefaf59da0bd8
choii4924 <choii4924@naver.com>
2025-09-02 15:15:39 +0900

    Add files via upload

---
+++ practice1/src/com/kh/a_basic/OperatorPractice.java
@@ -0,0 +1,21 @@
+package com.kh.a_basic;
+
+import java.util.Scanner;
+
+public class OperatorPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void method1() {
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.print("하나의 정수가 양수이면 양수다 : ");
+	char ch = sc.next().charAt(0);
+	
+	String result = ch == '-9' || ch == '-9' ? "." : "다.";
+	
+	
+	}
+}
